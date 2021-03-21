49e0791f19c0c69565473067b835fcc10fcaba45
Shay Wagner <srwagner916@gmail.com>
2021-03-20 20:49:15 -0500

    variables are now declared correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
-var upperCase = lowerCase.toUpperCase();
-var numbers = "1 2 3 4 5 6 7 8 9";
-var symbols = "! @ # $ % ^ & * ?";
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+var passwordArr = [];
+
+var generatePassword = function(){
+  var length = prompt("How many characters would you like? Please enter a number between 8 and 128");
+  if(length >= 8 && length <= 128) {
+    console.log(length);
+  }
+  else {
+    alert("Please enter a valid answer.");
+    generatePassword();
+  }
+  var includeLowercase = prompt("Would you like to include lowercase characters? Enter 'yes' or 'no'.");
+  if (includeLowercase === "yes" || includeLowercase === "YES");
+    var passCharacters = passwordArr.concat(lowercase);
+    console.log(passCharacters);
+};
+
 
 
 // Write password to the #password input
@@ -18,60 +35,56 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var generatePassword = function() {
-  generateLength();
-  generateLowerCase();
-  generateUpperCase();
-  generateSpecialCharacter();
-  console.log(length);
-};
 
 //Create functions for generating length and characters
 // 
 // 
 // 
 
-var generateLength = function() {
-  var length = prompt("How many characters would you like? Please enter a number between 8 and 128");
-  if(length >= 8 && length <= 128) {
-    return parseInt(length);  
-  }
-  else {
-    alert("Please enter a valid answer.");
-    generateLength();
-  }
-};
+// var generateLength = function() {
+//   var length = prompt("How many characters would you like? Please enter a number between 8 and 128");
+//   if(length >= 8 && length <= 128) {
+//     return parseInt(length);  
+//   }
+//   else {
+//     alert("Please enter a valid answer.");
+//     generateLength();
+//   }
+// };
 
-var generateLowerCase = function() {
-  var includeLowerCase = prompt("Would you like to include lower case characters? Please enter 'yes' or 'no'.");
-  if (includeLowerCase === "yes" || includeLowerCase === "YES") {
-    return includeLowerCase;
-  }
-  else {
-    alert("Please enter a valid response.");
-    generateLowerCase();
-  }
-};
+// var generateLowerCase = function() {
+//   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+//   var includeLowerCase = prompt("Would you like to include lower case characters? Please enter 'yes' or 'no'.");
+//   if (includeLowerCase === "yes" || includeLowerCase === "YES") {
+//     console.log(lowerCase(Math.floor(Math.random()* lowerCase.length)))
+//     return lowerCase(Math.floor(Math.random()* lowerCase.length));
+    
+//   }
+//   else {
+//     alert("Please enter a valid response.");
+//     generateLowerCase();
+//   }
+// };
 
-var generateUpperCase = function() {
-  var includeUpperCase = prompt("Would you like to include upper case characters? Please enter 'yes' or 'no'.");
-  if (includeUpperCase === "yes" || includeUpperCase === "YES") {
-    return includeUpperCase;
-  }
-  else {
-    alert("Please enter a valid response");
-    generateUpperCase();
-  }
-};
+// var generateUpperCase = function() {
+//   var includeUpperCase = prompt("Would you like to include upper case characters? Please enter 'yes' or 'no'.");
+//   if (includeUpperCase === "yes" || includeUpperCase === "YES") {
+//     return includeUpperCase;
+//   }
+//   else {
+//     alert("Please enter a valid response");
+//     generateUpperCase();
+//   }
+// };
 
-var generateSpecialCharacter = function() {
-  var includeSpecialCharacter = prompt("Would you like to include special characters? Please enter 'yes' or 'no'.");
-  if (includeSpecialCharacter === "yes" || includeSpecialCharacter === "YES") {
-    return includeSpecialCharacter;
-  }
-  else {
-    alert("Please enter a valid response");
-    generateSpecialCharacter();
-  }
-};
+// var generateSpecialCharacter = function() {
+//   var includeSpecialCharacter = prompt("Would you like to include special characters? Please enter 'yes' or 'no'.");
+//   if (includeSpecialCharacter === "yes" || includeSpecialCharacter === "YES") {
+//     return includeSpecialCharacter;
+//   }
+//   else {
+//     alert("Please enter a valid response");
+//     generateSpecialCharacter();
+//   }
+// };
 
